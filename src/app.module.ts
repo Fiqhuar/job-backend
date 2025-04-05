28d886f9e1d546104176394da828867e870d4240
Fiazkhan <fiazaikido@example.com>
2025-04-05 16:17:50 +0530

    fix: added env fallbacks for DB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,7 +25,7 @@ import { Job } from './job/job.entity';
       host: process.env.DB_HOST || 'localhost',
       port: parseInt(process.env.DB_PORT || '5432', 10),
       username: process.env.DB_USER || 'postgres',
-      password: process.env.DB_PASS || '',
+      password: process.env.DB_PASS || 'root',
       database: process.env.DB_NAME || 'postgres',
       entities: [Job],
       synchronize: false,
